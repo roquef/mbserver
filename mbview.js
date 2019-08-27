f3986716e79c486a4ae56c1a3626722d795c3b99
Roque Francisco <roquefsd@gmail.com>
2019-08-26 21:56:52 -0300

    accepts png

--- mbview.js
+++ mbview.js
@@ -13,74 +13,84 @@ app.use(express.static('public'));
 
 module.exports = {
 
-  /**
-   * Load a tileset and return a reference with metadata
-   * @param {object} file reference to the tileset
-   * @param {function} callback that returns the resulting tileset object
-   */
-  loadTiles: function (file, callback) {
-    new MBTiles(file, ((err, tiles) => {
-      if (err) throw err;
-      tiles.getInfo((err, info) => {
-        if (err) throw err;
+	/**
+	 * Load a tileset and return a reference with metadata
+	 * @param {object} file reference to the tileset
+	 * @param {function} callback that returns the resulting tileset object
+	 */
+	loadTiles: function (file, callback) {
+		new MBTiles(file, ((err, tiles) => {
+			if (err) throw err;
+			tiles.getInfo((err, info) => {
+				if (err) throw err;
 
-        const tileset = objectAssign({}, info, {
-          tiles: tiles
-        });
+				const tileset = objectAssign({}, info, {
+					tiles: tiles
+				});
 
-        callback(null, tileset);
-      });
-    }));
-  },
+				callback(null, tileset);
+			});
+		}));
+	},
 
-  /**
-  * Defer loading of multiple MBTiles and spin up server.
-  * Will merge all the configurations found in the sources.
-  * @param {object} config for the server, e.g. port
-  * @param {function} callback with the server configuration loaded
-  */
-  serve: function (config, callback) {
-    const loadTiles = this.loadTiles;
-    const listen = this.listen;
+	/**
+	* Defer loading of multiple MBTiles and spin up server.
+	* Will merge all the configurations found in the sources.
+	* @param {object} config for the server, e.g. port
+	* @param {function} callback with the server configuration loaded
+	*/
+	serve: function (config, callback) {
+		const loadTiles = this.loadTiles;
+		const listen = this.listen;
 
-    config.mbtiles.forEach((file) => {
-      q.defer(loadTiles, file);
-    });
+		config.mbtiles.forEach((file) => {
+			q.defer(loadTiles, file);
+		});
 
-    q.awaitAll((error, tilesets) => {
-      if (error) throw error;
-      const finalConfig = utils.mergeConfigurations(config, tilesets);
-      listen(finalConfig, callback);
-    });
-  },
+		q.awaitAll((error, tilesets) => {
+			if (error) throw error;
+			const finalConfig = utils.mergeConfigurations(config, tilesets);
+			listen(finalConfig, callback);
+		});
+	},
 
-  listen: function (config, onListen) {
-    const format = config.tiles._info.format;
+	listen: function (config, onListen) {
+		const format = config.tiles._info.format;
 
-    app.get('/preview', (req, res) => {
-      if (format === 'pbf') {
-        res.render('vector', config);
-      } else {
-        res.render('raster', config);
-      }
-    });
+		app.get('/preview', (req, res) => {
+			res.render('vector', config);
+		});
 
-    app.get('/:source/:z/:x/:y.' + format, (req, res) => {
-      const p = req.params;
+		app.get('/:source/:z/:x/:y.pbf', (req, res) => {
+			const p = req.params;
 
-      const tiles = config.sources[p.source].tiles;
-      tiles.getTile(p.z, p.x, p.y, (err, tile, headers) => {
-        if (err) {
-          res.end();
-        } else {
-          res.writeHead(200, headers);
-          res.end(tile);
-        }
-      });
-    });
+			const tiles = config.sources[p.source].tiles;
+			tiles.getTile(p.z, p.x, p.y, (err, tile, headers) => {
+				if (err) {
+					res.end();
+				} else {
+					res.writeHead(200, headers);
+					res.end(tile);
+				}
+			});
+		});
 
-    config.server = app.listen(config.port, () => {
-      onListen(null, config);
-    });
-  }
+		app.get('/:source/:z/:x/:y.png', (req, res) => {
+			const p = req.params;
+
+			const tiles = config.sources[p.source].tiles;
+			tiles.getTile(p.z, p.x, p.y, (err, tile, headers) => {
+				if (err) {
+					res.end();
+				} else {
+					res.writeHead(200, headers);
+					res.end(tile);
+				}
+			});
+		});
+
+		config.server = app.listen(config.port, () => {
+			onListen(null, config);
+		});
+	}
 };
